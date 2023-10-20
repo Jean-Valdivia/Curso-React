fe126b9ed7f0aa60f9ad4952f3add863deff9d85
Jean Valdivia <jeanvaldivia96@gmail.com>
2023-10-20 02:04:01 -0300

    cambios entrega final

--- fenix3d/src/components/products/ProductDetail.jsx
+++ fenix3d/src/components/products/ProductDetail.jsx
@@ -1,7 +1,6 @@
 import { Card, CardContent, Grid, Typography } from "@mui/material";
 import ProductInfo from "./ProductInfo";
 import { useEffect, useState } from "react";
-import { doc, getDoc, getFirestore } from 'firebase/firestore'
 
 const ProductDetail = ({ product, children }) => {
     const { id, image, title, price, itHadDues, isAnOffer, stock } = product
@@ -16,7 +15,7 @@ const ProductDetail = ({ product, children }) => {
 
     return (<>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Card className="card-products-container" onClick={handleClick} style={}>
+            <Card className="card-products-container" onClick={handleClick}>
                 <img src={image} />
                 <CardContent className="card-products-content">
                     <Typography>{title}</Typography>
--- fenix3d/src/main.jsx
+++ fenix3d/src/main.jsx
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCjgQUUODOSzOCtCe7NA9E1f8q9c0B77aE",
-  authDomain: "fenix3d-ae9d6.firebaseapp.com",
-  projectId: "fenix3d-ae9d6",
-  storageBucket: "fenix3d-ae9d6.appspot.com",
-  messagingSenderId: "925732073920",
-  appId: "1:925732073920:web:ed5bb12d224a34867d9825",
-  measurementId: "G-MTKW6E56KG"
-};
-
-export const app = initializeApp(firebaseConfig);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
